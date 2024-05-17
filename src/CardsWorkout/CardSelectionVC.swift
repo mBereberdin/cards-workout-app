897a3efc010bf9c65dd975574775a781a4872e69
Mikhail Bereberdin <bereberdin999@icloud.com>
2024-05-17 19:25:06 +0700

    Добавление UI элементов для контроллера карт
    - Добавлены UI элементы;
    - реализованы методы для конфигурирования добавленных UI элементов.

--- src/CardsWorkout/CardSelectionVC.swift
+++ src/CardsWorkout/CardSelectionVC.swift
@@ -9,14 +9,75 @@ import UIKit
 
 class CardSelectionVC: UIViewController {
     
+    let cardImageView = UIImageView()
+    
+    let stopButton = CWButton(backgroundColor: .systemRed, title: "Stop!")
+    let restartButton = CWButton(backgroundColor: .systemGreen, title: "Restart")
+    let rulesButton = CWButton(backgroundColor: .systemBlue, title: "Rules")
+    
     var cards: [UIImage] = Card.allValues
     
     var timer: Timer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        configureUI()
         
         startTimer()
+    }
+    
+    func configureUI() {
+        configureCardImageView()
+        configureStopButton()
+        configureRestartButton()
+        configureRulesButton()
+    }
+    
+    func configureCardImageView() {
+        view.addSubview(cardImageView)
+        cardImageView.translatesAutoresizingMaskIntoConstraints = false
+        cardImageView.image = UIImage(named: "AS")
+        
+        NSLayoutConstraint.activate([
+            cardImageView.widthAnchor.constraint(equalToConstant: 250),
+            cardImageView.heightAnchor.constraint(equalToConstant: 350),
+            cardImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            cardImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -75)
+        ])
+    }
+    
+    func configureStopButton() {
+        view.addSubview(stopButton)
+        
+        NSLayoutConstraint.activate([
+            stopButton.widthAnchor.constraint(equalToConstant: 260),
+            stopButton.heightAnchor.constraint(equalToConstant: 50),
+            stopButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stopButton.topAnchor.constraint(equalTo: cardImageView.bottomAnchor, constant: 30)
+        ])
+    }
+    
+    func configureRestartButton() {
+        view.addSubview(restartButton)
+        
+        NSLayoutConstraint.activate([
+            restartButton.widthAnchor.constraint(equalToConstant: 115),
+            restartButton.heightAnchor.constraint(equalToConstant: 50),
+            restartButton.leadingAnchor.constraint(equalTo: stopButton.leadingAnchor),
+            restartButton.topAnchor.constraint(equalTo: stopButton.bottomAnchor, constant: 20)
+        ])
+    }
+    
+    func configureRulesButton() {
+        view.addSubview(rulesButton)
+        
+        NSLayoutConstraint.activate([
+            rulesButton.widthAnchor.constraint(equalToConstant: 115),
+            rulesButton.heightAnchor.constraint(equalToConstant: 50),
+            rulesButton.trailingAnchor.constraint(equalTo: stopButton.trailingAnchor),
+            rulesButton.topAnchor.constraint(equalTo: stopButton.bottomAnchor, constant: 20)
+        ])
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -31,5 +92,5 @@ class CardSelectionVC: UIViewController {
     
     @objc func showRandomImage() {
         cardImageView.image = cards.randomElement() ?? UIImage(named: "AS")
-    }    
+    }
 }
