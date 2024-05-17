a4584393c007ec09dbd7ebb5edac10553759c8d7
Mikhail Bereberdin <bereberdin999@icloud.com>
2024-05-17 19:23:05 +0700

    Добавление класса кнопок приложения

---
+++ src/CardsWorkout/CWButton.swift
@@ -0,0 +1,34 @@
+//
+//  CWButton.swift
+//  CardsWorkout
+//
+//  Created by Михаил Беребердин on 17.05.2024.
+//
+
+import UIKit
+
+class CWButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        setTitle(title, for: .normal)
+        configure()
+    }
+    
+    func configure() {
+        layer.cornerRadius = 8
+        titleLabel?.font = .systemFont(ofSize: 19, weight: .bold)
+        setTitleColor(.label, for: .normal)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
