914a32115be68df804257476eabff91ace9bb5db
Mikhail Bereberdin <bereberdin999@icloud.com>
2024-05-17 11:38:01 +0700

    Добавление экрана с правилами
    - добавлен контроллер с правилами;
    - добавлена коллекция кнопок для настройки их отображения.

--- src/CardsWorkout/CardSelectionVC.swift
+++ src/CardsWorkout/CardSelectionVC.swift
@@ -8,22 +8,22 @@
 import UIKit
 
 class CardSelectionVC: UIViewController {
-
+    
+    @IBOutlet var cardImageView: UIImageView!
+    @IBOutlet var buttons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        for button in buttons {
+            button.layer.cornerRadius = 8
+        }
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func stopButtonTapped(_ sender: Any) {
     }
-    */
-
+    
+    @IBAction func restartButtonTapped(_ sender: UIButton) {
+    }
 }
