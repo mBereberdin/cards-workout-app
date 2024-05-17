488a4e8ef287740f23fc47fa0293d117839d51b2
Mikhail Bereberdin <bereberdin999@icloud.com>
2024-05-17 14:42:48 +0700

    Добавление карт
    - Добавлено использование карт;
    - добавлено отображение карт по таймеру;
    - добавлены оставнока и перезапуск таймера с отображением карт.

--- src/CardsWorkout/CardSelectionVC.swift
+++ src/CardsWorkout/CardSelectionVC.swift
@@ -11,19 +11,40 @@ class CardSelectionVC: UIViewController {
     
     @IBOutlet var cardImageView: UIImageView!
     @IBOutlet var buttons: [UIButton]!
+    var cards: [UIImage] = Card.allValues
+    
+    var timer: Timer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        startTimer()
+        
         for button in buttons {
             button.layer.cornerRadius = 8
         }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
         
+        timer.invalidate()
+    }
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(showRandomImage), userInfo: nil, repeats: true)
+    }
+    
+    @objc func showRandomImage() {
+        cardImageView.image = cards.randomElement() ?? UIImage(named: "AS")
     }
     
     @IBAction func stopButtonTapped(_ sender: Any) {
+        timer.invalidate()
     }
     
     @IBAction func restartButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
+        startTimer()
     }
 }
